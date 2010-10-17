6a69c5579250d3c14698f0c80e0f0edf729681fa
Joe Pletcher <joepletcher@gmail.com>
2010-10-16 21:37:36 -0700

    Adding functions, still worthless.

---
+++ client.c
@@ -0,0 +1,10 @@
+#define DEBUG 1
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "raw.h"
+
+int main() {
+
+	return 0;
+}
---
+++ client.h
@@ -0,0 +1,6 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+int ui_loop();
+
+#endif
